a720f843adccca9775f5907b3e77c5d41a254bb9
Hari Prasad Poudel <haripd49poudel24@gmail.com>
2024-02-05 13:13:55 +0545

    update the id 2

--- src/main/java/com/jpa/SpringBootJpaApplication.java
+++ src/main/java/com/jpa/SpringBootJpaApplication.java
@@ -25,16 +25,30 @@ public class SpringBootJpaApplication {
 			VideoRepo videoRepo
 	){
 		return args -> {
-			for (int i = 0; i < 50; i++){
+			/*for (int i = 0; i < 50; i++){
 				Faker faker =new Faker();
 				var author = Author.builder()
 						.firstName(faker.name().firstName())
 						.lastName(faker.name().lastName())
 						.age(String.valueOf(faker.number().numberBetween(15, 45)))
-						.email("smith" +i+ "@gmail.com")
+						.email(faker.name().username() + "@gmail.com")
 						.build();
 				authorRepo.save(author);
-		}
+		}*/
+		
+//			update author with ID =2
+			var author = Author.builder()
+					.id(2l)
+					.firstName("Ramesh")
+					.lastName("Poudel")
+					.age(String.valueOf(23))
+					.email("ramesh@gmail.com")
+					.build();
+					authorRepo.save(author);
+			
+			
+			
+			
 			/*var video = Video.builder()
 					.name("Nepal")
 					.length(5l)
